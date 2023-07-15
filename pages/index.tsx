9dece9232e59ac41d449ecab966b1ad2e24d38fb
vlad <84715178+vladimirlarass@users.noreply.github.com>
2023-07-15 11:38:48 +0100

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/navbar";
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <>
+    <div>
     <Navbar/>
-    <h1>Bienvenue sur le site de <strong>Vladimir larass</strong></h1><p>je suis un developpeur web Full Stack</p>
-    </>
-    
-      
-    
-  )
+    <h1 className={styles.title}>Bienvenue sur le site de <strong>Vladimir larass</strong>
+    </h1><p>je suis un developpeur web Full Stack</p>
+    </div>
+  ) 
 }
